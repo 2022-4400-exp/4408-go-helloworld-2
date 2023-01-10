abeb75c66efc3e7feb3305f99b614f8c15bb85aa
031201 <m19028@g.metro-cit.ac.jp>
2023-01-10 13:59:19 +0900

    first commit

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+    "net/http"
+    "github.com/labstack/echo"
+)
+
+func main() {
+    e := echo.New()
+
+    e.GET("/hello", Hello())
+    e.GET("/api/hello", ApiHelloGet())
+
+    e.Start(":8080")
+}
+
+func Hello() echo.HandlerFunc {
+    return func(c echo.Context) error {     
+        return c.String(http.StatusOK, "学生番号: 一言")
+    }
+}
+func Hello() echo.HandlerFunc {
+    return func(c echo.Context) error {     
+        return c.String(http.StatusOK, "4408: Hello ver.2")
+    }
+}
